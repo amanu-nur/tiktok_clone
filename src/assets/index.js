c37522207e8052c2417db7fdb9bb4ccee30a3c45
Amanu Nur A <amanunura@Amanus-MacBook-Pro.local>
2021-07-08 10:27:47 +0700

    frist-commit

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import IcAdd from './add.png';
+import IcBtnPlus from './btn_plus.png';
+import IcComents from './coments.png';
+import IcDics from './Disc.png';
+import IcHome from './home.png';
+import IcLike from './like.png';
+import IcNotif from './message.png';
+import IcProfile from './profile.png';
+import IcSearch from './search.png';
+import IcShare from './share.png';
+
+export {
+  IcAdd,
+  IcBtnPlus,
+  IcComents,
+  IcDics,
+  IcHome,
+  IcLike,
+  IcNotif,
+  IcProfile,
+  IcSearch,
+  IcShare,
+};
